1e790caeb0665aebe51046a43092d04bf2337ace
Aleksey Zubkov <zubkovf@mail.ru>
2022-01-21 01:35:51 +0700

    Code refactoring

--- src/view.js
+++ src/view.js
@@ -13,42 +13,15 @@ const render = (state, i18nextInstance) => {
 
   const watchedState = onChange(state, (path, value) => {
     if (path === 'rssForm.state') {
-      if (value === 'invalid') {
-        urlInput.classList.add('is-invalid');
-        feedback.classList.remove('text-success');
-        feedback.classList.add('text-danger');
-        feedback.textContent = i18nextInstance.t('feedback.invalid');
-      }
-      if (value === 'empty') {
-        urlInput.classList.add('is-invalid');
-        feedback.classList.remove('text-success');
-        feedback.classList.add('text-danger');
-        feedback.textContent = i18nextInstance.t('feedback.empty');
-      }
-
+      feedback.textContent = i18nextInstance.t(`feedback.${value}`);
       if (value === 'added') {
         urlInput.classList.remove('is-invalid');
         feedback.classList.remove('text-danger');
         feedback.classList.add('text-success');
-        feedback.textContent = i18nextInstance.t('feedback.added');
-      }
-      if (value === 'exists') {
+      } else {
         urlInput.classList.add('is-invalid');
         feedback.classList.remove('text-success');
         feedback.classList.add('text-danger');
-        feedback.textContent = i18nextInstance.t('feedback.exists');
-      }
-      if (value === 'network error') {
-        urlInput.classList.add('is-invalid');
-        feedback.classList.remove('text-success');
-        feedback.classList.add('text-danger');
-        feedback.textContent = i18nextInstance.t('feedback.network error');
-      }
-      if (value === 'invalid RSS') {
-        urlInput.classList.add('is-invalid');
-        feedback.classList.remove('text-success');
-        feedback.classList.add('text-danger');
-        feedback.textContent = i18nextInstance.t('feedback.invalid RSS');
       }
     }
     if (path === 'rssForm.feeds') {
